3e5a0ae67f9df627d1f5698f340bfeeba0a18f04
diogofnmachado <diogofnandi@gmail.com>
2017-05-07 22:45:24 -0300

    07/05 Claviculario eletronico
    Resolver comentários

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/entidades/EventoClaviculario.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/entidades/EventoClaviculario.java
@@ -13,4 +13,44 @@ public class EventoClaviculario {
 
     private String placa;
 
+    public EventoClaviculario(Evento evento, Calendar datahora, int matricula, String placa) {
+        this.evento = evento;
+        this.datahora = datahora;
+        this.matricula = matricula;
+        this.placa = placa;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public Calendar getDatahora() {
+        return datahora;
+    }
+
+    public void setDatahora(Calendar datahora) {
+        this.datahora = datahora;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+    
 }
