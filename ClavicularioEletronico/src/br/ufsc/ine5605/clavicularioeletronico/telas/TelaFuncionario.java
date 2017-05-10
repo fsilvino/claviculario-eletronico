886dc796aaa47f980cacb11af982ff3129b04768
gabrielcargnin <gabrielcargnin@hotmail.com>
2017-05-10 04:57:27 -0300

    Telas
    Cadastros (Inclui, exclui, altera, lista) testados e operantes.

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaFuncionario.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaFuncionario.java
@@ -1,20 +1,134 @@
 package br.ufsc.ine5605.clavicularioeletronico.telas;
 
-public class TelaFuncionario extends TelaCadastro {
+import br.ufsc.ine5605.clavicularioeletronico.controladores.ControladorFuncionario;
+import br.ufsc.ine5605.clavicularioeletronico.enums.Cargo;
+import br.ufsc.ine5605.clavicularioeletronico.transferencias.DadosFuncionario;
+import br.ufsc.ine5605.clavicularioeletronico.transferencias.ItemListaCadastro;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class TelaFuncionario extends TelaCadastro {
+    
+    @Override
+    public void exibeMenu() {
+        System.out.println("----------------Menu Funcionario-----------------");
+        super.exibeMenu();
+    }
+    
     @Override
     public void exibeTelaInclui() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DadosFuncionario novoFuncionario = entradaDadosFuncionario();
+        
+        try {
+            ControladorFuncionario.getInstance().inclui(novoFuncionario);
+            System.out.println("Funcionário cadastrado com sucesso!");
+        
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void exibeTelaAltera() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DadosFuncionario alteraFuncionario = entradaDadosFuncionario();
+        
+        try {
+            ControladorFuncionario.getInstance().inclui(alteraFuncionario);
+            System.out.println("Funcionário alterado com sucesso!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void exibeTelaExclui() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            ControladorFuncionario.getInstance().exclui(inputMatricula());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @Override
+    public void exibeLista() {
+        System.out.println("------------Funcionarios cadastrados-------------");
+        for (ItemListaCadastro item: ControladorFuncionario.getInstance().getListaItensCadastro()) {
+            System.out.println(item.getDescricao());
+        }
+        System.out.println("-------------------------------------------------");
+    }
+    
+    public Date dataNascimento() {
+        System.out.println("Digite a data de nascimento do funcionário (dd/mm/aaaa): ");
+        String data = leitor.nextLine();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date nascimento = null;
+        try {
+            nascimento = formato.parse(data);
+        } catch (ParseException ex) {
+            System.out.println("Você deve digitar uma data no formato dd/mm/aaaa!");
+            dataNascimento();
+        }
+        return nascimento;
+    }
+    
+    public DadosFuncionario entradaDadosFuncionario() {
+        return new DadosFuncionario(inputMatricula(), inputNome(), inputDataNascimento(), inputTelefone(), inputCargo(), false);
+    }
+    
+   private int inputMatricula() {
+        System.out.println("Digite a matricula: ");
+        String input = leitor.nextLine();
+        int matricula = -1;
+        if (input.matches("[0-9]")) {
+            matricula = Integer.parseInt(input);
+        }
+        return matricula;
+    }
+    
+    private String inputNome() {
+        System.out.println("Digite o nome do funcionario: ");
+        return leitor.nextLine();
+    }
+    
+    private Date inputDataNascimento() {
+        System.out.println("Digite a data de nascimento do funcionário (dd/mm/aaaa): ");
+        String data = leitor.nextLine();
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date nascimento = null;
+        try {
+            nascimento = formato.parse(data);
+        } catch (ParseException e) {            
+        }
+        
+        return nascimento;
+    }
+    
+    private String inputTelefone() {
+        System.out.println("Digite o telefone do funcionário: ");
+        return leitor.nextLine();
+        
+    }
+
+    
+    
+    private Cargo inputCargo(){
+        System.out.println("Digite o cargo do funcionário");
+        System.out.println("1 - Diretoria");       
+        System.out.println("2 - Motorista:");
+        String input = leitor.nextLine();
+        int opcao = 0;
+        if (input.matches("[0-9]")) {
+            opcao = Integer.parseInt(input);
+        }
+        switch (opcao) {
+            case 1: return Cargo.DIRETORIA;
+            
+            case 2: return Cargo.MOTORISTA;
+        }   
+        
+        return null;
     }
 
 }
--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaVeiculo.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaVeiculo.java
@@ -1,20 +1,107 @@
 package br.ufsc.ine5605.clavicularioeletronico.telas;
 
+import br.ufsc.ine5605.clavicularioeletronico.controladores.ControladorVeiculo;
+import br.ufsc.ine5605.clavicularioeletronico.transferencias.DadosVeiculo;
+import br.ufsc.ine5605.clavicularioeletronico.transferencias.ItemListaCadastro;
+
 public class TelaVeiculo extends TelaCadastro {
 
     @Override
+    public void exibeMenu() {
+        System.out.println("----------------Menu Veiculo-----------------");
+        super.exibeMenu();
+    }
+    
+    @Override
     public void exibeTelaInclui() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        DadosVeiculo novoVeiculo = entradaDadosVeiculo();
+        
+        try {
+            ControladorVeiculo.getInstance().inclui(novoVeiculo);
+            System.out.println("Veiculo cadastrado com sucesso!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void exibeTelaAltera() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DadosVeiculo alteraVeiculo = entradaDadosVeiculo();
+        
+        try {
+            ControladorVeiculo.getInstance().altera(alteraVeiculo);
+            System.out.println("Veiculo alterado com sucesso!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }        
     }
 
     @Override
     public void exibeTelaExclui() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            ControladorVeiculo.getInstance().exclui(inputPlaca());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @Override
+    public void exibeLista() {
+        System.out.println("--------------Veiculos cadastrados---------------");
+        for (ItemListaCadastro item: ControladorVeiculo.getInstance().getListaItensCadastro()) {
+            System.out.println(item.getDescricao());
+        }
+        System.out.println("-------------------------------------------------");
+    }
+    
+    private DadosVeiculo entradaDadosVeiculo() {
+        return new DadosVeiculo(inputPlaca(), inputModelo(), inputMarca(), inputAno(), inputQuilometragemAtual());
+    }
+    
+    private String inputPlaca() {
+        System.out.println("Digite a placa do veiculo. Ex: (AAA-9999): ");
+        String placa = leitor.nextLine();
+        //if (!placa.matches("[A-Z]{3}-{1}\\d{4}")) {
+        //    System.out.println("A placa deve ser digitada no seguinte modelo: AAA-9999");
+        //}
+        return placa;
+    }
+    
+    private String inputModelo() {
+        System.out.println("Digite o modelo do veiculo: ");
+        return leitor.nextLine();
+    }
+    
+    private String inputMarca() {
+        System.out.println("Digite o marca do veiculo: ");
+        return leitor.nextLine();
+    }
+    
+    private int inputAno(){
+        System.out.println("Digite o ano do veiculo: ");
+        String input = leitor.nextLine();
+        int ano = 0;
+        if (input.matches("[0-9]{4}")) {
+            ano = Integer.parseInt(input);
+        }
+        //else {
+        //    throw new Exception("Você deve informar um numero!");
+        //}
+        return ano;
+    }
+    
+    private int inputQuilometragemAtual() {
+        System.out.println("Digite a quilometragem atual do veiculo: ");
+        String input = leitor.nextLine();
+        int quilometragemAtual = 0;
+        if (input.matches("[0-9]")) {
+            quilometragemAtual = Integer.parseInt(input);
+        }
+        //else {
+        //    throw new Exception("Você deve informar um numero!");
+        //}
+        return quilometragemAtual;
     }
 
 }
