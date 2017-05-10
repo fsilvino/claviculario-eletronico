efcb7eda0f47a5653b1eb598001344c009beaff0
gabrielcargnin <gabrielcargnin@hotmail.com>
2017-05-10 17:27:01 -0300

    Tela Permissão
    Tirando a opção de alterar permissao

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaPermissaoUsoVeiculo.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaPermissaoUsoVeiculo.java
@@ -6,10 +6,45 @@ import java.util.List;
 
 /**
  *
- * @author Flávio
+ * @author Gabriel
  */
 public class TelaPermissaoUsoVeiculo extends TelaCadastro {
+    
+    @Override
+    public void exibeMenu() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("----------------Menu Permissoes-----------------");
+            System.out.println("1 - Incluir");       
+            System.out.println("2 - Excluir");
+            System.out.println("3 - Listar cadastros");
+            System.out.println("0 - Voltar ao menu inicial");
+            System.out.println("-------------------------------------------------");
+            System.out.print("Opção escolhida: ");
+            
+            try {
+                opcao = Integer.parseInt(this.teclado.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("A opcao precisa ser um numero!");
+            }
+            if (opcao > 3 || opcao < 0) {
+                System.out.println("Informe uma opcao valida!");
+            }
+            else {
+                switch (opcao) {
+                    case 1: exibeTelaInclui();
+                            break;
 
+                    case 2: exibeTelaExclui();
+                            break;
+                            
+                    case 3: exibeLista();
+                            break;
+                }
+            }
+        }             
+    }
+    
     @Override
     public void exibeTelaInclui() {
         try {
