70f8bff7c3e9d35fd04e0e9319b502c1e2160a00
fsilvino <fsilvino@users.noreply.github.com>
2017-05-12 19:10:10 -0300

    sd

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaClaviculario.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaClaviculario.java
@@ -17,8 +17,12 @@ public class TelaClaviculario extends TelaBase {
             System.out.println("3) Relatorios");
             System.out.println("0) Voltar ao menu inicial");
         
-            if (this.teclado.hasNextInt()) {
-                opcao = this.teclado.nextInt();
+            try {
+                opcao = Integer.parseInt(this.teclado.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("A opcao precisa ser um numero!");
+            }
+            if (opcao < 3 || opcao > 0) {
                 switch (opcao) {
                     case 1:
                         retirarChave();
@@ -27,7 +31,63 @@ public class TelaClaviculario extends TelaBase {
                         devolverChave();
                         break;
                     case 3:
-                        relatorios();
+                        exibeMenuRelatorios();
+                        break;
+                }
+            } else {
+                System.out.println("Informe uma opcao valida!");
+            }
+        }
+    }
+    
+    private void exibeMenuRelatorios() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("----------------Relatorios-----------------");
+            System.out.println("1) Exibe relatorio completo");
+            System.out.println("2) Pesquisa por funcionario");
+            System.out.println("3) Pesquisa por veiculo ");
+            System.out.println("4) Pesquisa por evento");
+            System.out.println("0) Sair");
+        
+            if (this.teclado.hasNextInt()) {
+                opcao = this.teclado.nextInt();
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        exibeMenuEventos();
+                        break;
+                }
+            } else {
+                System.out.println("Informe uma opcao valida!");
+            }
+        }
+    }
+    
+    private void exibeMenuEventos() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("-----------Selecione um evento---------------");
+            System.out.println("1) Acesso permitido");
+            System.out.println("2) Acesso bloqueado");
+            System.out.println("3) Usuario bloqueado");
+            System.out.println("4) Matricula invalida");
+            System.out.println("5) Permissao insuficiente");
+            System.out.println("6) Veiculo indisponivel");
+            System.out.println("7) Veiculo devolvido");
+            System.out.println("0) Sair");
+        
+            if (this.teclado.hasNextInt()) {
+                opcao = this.teclado.nextInt();
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
                         break;
                 }
             } else {
@@ -38,22 +98,30 @@ public class TelaClaviculario extends TelaBase {
     
     private void retirarChave() {
         try {
-            ControladorClaviculario.getInstance().retirarChave();
+            ControladorClaviculario.getInstance().retirarChave(inputMatricula());
             System.out.println("Chave liberada com sucesso!");
-            solicitaEnterParaContinuar();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        solicitaEnterParaContinuar();
     }
     
     private void devolverChave() {
-        
+        try {
+            ControladorClaviculario.getInstance().devolverVeiculo(
+                    inputMatricula(), inputPlaca(), inputQuilometragemAtual()
+            );
+            System.out.println("Chave devolvida com sucesso!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        solicitaEnterParaContinuar();
     }
     
-    private void relatorios() {
+    private void exibeRelatorios (List<ItemListaCadastro> relatorio) {
         
     }
-    
+        
     public int pedeMatricula() {
         System.out.println("Matricula: ");
         if (this.teclado.hasNextInt()) {
