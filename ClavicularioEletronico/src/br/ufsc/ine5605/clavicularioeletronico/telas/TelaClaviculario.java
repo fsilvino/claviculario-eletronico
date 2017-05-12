76ac90dcb84ed1d7f4b3f15b8b85479e1c89d16f
diogofnmachado <diogofnandi@gmail.com>
2017-05-12 17:04:35 -0300

    Tela claviculario
    -relatorios
    -devolver chave

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaClaviculario.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaClaviculario.java
@@ -1,6 +1,8 @@
 package br.ufsc.ine5605.clavicularioeletronico.telas;
 
 import br.ufsc.ine5605.clavicularioeletronico.controladores.ControladorClaviculario;
+import br.ufsc.ine5605.clavicularioeletronico.transferencias.ItemListaCadastro;
+import java.util.List;
 import java.util.Scanner;
 
 public class TelaClaviculario extends TelaBase {
@@ -28,7 +30,63 @@ public class TelaClaviculario extends TelaBase {
                         devolverChave();
                         break;
                     case 3:
-                        relatorios();
+                        exibeMenuRelatorios();
+                        break;
+                }
+            } else {
+                System.out.println("Informe uma opcao valida!");
+            }
+        }
+    }
+    
+    private void exibeMenuRelatorios() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("----------------Relatorios-----------------");
+            System.out.println("1) Exibe relatorio completo");
+            System.out.println("2) Pesquisa por funcionario");
+            System.out.println("3) Pesquisa por veiculo ");
+            System.out.println("4) Pesquisa por evento");
+            System.out.println("0) Sair");
+        
+            if (this.teclado.hasNextInt()) {
+                opcao = this.teclado.nextInt();
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        exibeMenuEventos();
+                        break;
+                }
+            } else {
+                System.out.println("Informe uma opcao valida!");
+            }
+        }
+    }
+    
+    private void exibeMenuEventos() {
+        int opcao = -1;
+        while (opcao != 0) {
+            System.out.println("-----------Selecione um evento---------------");
+            System.out.println("1) Acesso permitido");
+            System.out.println("2) Acesso bloqueado");
+            System.out.println("3) Usuario bloqueado");
+            System.out.println("4) Matricula invalida");
+            System.out.println("5) Permissao insuficiente");
+            System.out.println("6) Veiculo indisponivel");
+            System.out.println("7) Veiculo devolvido");
+            System.out.println("0) Sair");
+        
+            if (this.teclado.hasNextInt()) {
+                opcao = this.teclado.nextInt();
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
                         break;
                 }
             } else {
@@ -48,13 +106,13 @@ public class TelaClaviculario extends TelaBase {
     }
     
     private void devolverChave() {
-        
+    
     }
     
-    private void relatorios() {
+    private void exibeRelatorios (List<ItemListaCadastro> relatorio) {
         
     }
-    
+        
     public int pedeMatricula() {
         System.out.println("Matricula: ");
         if (this.teclado.hasNextInt()) {
