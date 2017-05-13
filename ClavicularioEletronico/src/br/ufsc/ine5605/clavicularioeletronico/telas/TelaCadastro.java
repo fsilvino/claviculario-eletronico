58fdd8be24ca0173117933089d493840a8c224be
fsilvino <fsilvino@users.noreply.github.com>
2017-05-12 23:51:20 -0300

    Alterações nas Telas

--- ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaCadastro.java
+++ ClavicularioEletronico/src/br/ufsc/ine5605/clavicularioeletronico/telas/TelaCadastro.java
@@ -14,38 +14,42 @@ public abstract class TelaCadastro extends TelaBase {
     
     public abstract void exibeLista();
 
+    protected boolean executaOpcaoMenu(int opcao) {
+        switch (opcao) {
+            case 1:
+                exibeTelaInclui();
+                break;
+            case 2:
+                exibeTelaAltera();
+                break;
+            case 3:
+                exibeTelaExclui();
+                break;
+            case 4:
+                exibeLista();
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+    
+    protected void exibeOpcoesMenu() {
+        System.out.println("1 - Incluir");
+        System.out.println("2 - Alterar");
+        System.out.println("3 - Excluir");
+        System.out.println("4 - Listagem");
+        System.out.println("0 - Voltar");
+    }
+    
     public void exibeMenu() {
         int opcao = -1;
         while (opcao != 0) {
-            System.out.println("1 - Incluir");       
-            System.out.println("2 - Alterar");
-            System.out.println("3 - Excluir");
-            System.out.println("4 - Listar cadastros");
-            System.out.println("0 - Voltar ao menu inicial");
-            System.out.println("-------------------------------------------------");
-            System.out.print("Opção escolhida: ");
-            
-            try {
-                opcao = Integer.parseInt(this.teclado.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("A opcao precisa ser um numero!");
-            }
-            if (opcao > 4 || opcao < 0) {
-                System.out.println("Informe uma opcao valida!");
-            }
-            else {
-                switch (opcao) {
-                    case 1: exibeTelaInclui();
-                            break;
-
-                    case 2: exibeTelaAltera();
-                            break;
-
-                    case 3: exibeTelaExclui();
-                            break;
-                            
-                    case 4: exibeLista();
-                            break;
+            exibeOpcoesMenu();
+            opcao = inputOpcao();
+            if (opcao != 0) {
+                if (executaOpcaoMenu(opcao)) {
+                    System.out.println("Informe uma opcao valida!");
                 }
             }
         }             
